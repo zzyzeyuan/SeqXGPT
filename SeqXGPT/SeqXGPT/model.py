bba0e92af29cc3dceed537173331c3c5c9c8bd66
Zeyuan Zhao <48199429+zzyzeyuan@users.noreply.github.com>
2023-12-25 11:21:19 +0800

    Update model.py

--- SeqXGPT/SeqXGPT/model.py
+++ SeqXGPT/SeqXGPT/model.py
@@ -178,4 +178,56 @@ class ModelWiseTransformerClassifier(nn.Module):
             pass
 
         return output
+
+class TransformerOnlyClassifier(nn.Module):
+
+    def __init__(self, id2labels, seq_len, embedding_size=4, num_heads=2, intermediate_size=64, num_layers=2, dropout_rate=0.1):
+        super(TransformerOnlyClassifier, self).__init__()
+
+        self.encoder_layer = TransformerEncoderLayer(
+            d_model=embedding_size,
+            nhead=num_heads,
+            dim_feedforward=intermediate_size,
+            dropout=dropout_rate,
+            batch_first=True)
+        self.encoder = TransformerEncoder(encoder_layer=self.encoder_layer,
+                                            num_layers=num_layers)
+
+        self.position_encoding = torch.zeros((seq_len, embedding_size))
+        for pos in range(seq_len):
+            for i in range(0, embedding_size, 2):
+                self.position_encoding[pos, i] = torch.sin(
+                    torch.tensor(pos / (10000**((2 * i) / embedding_size))))
+                self.position_encoding[pos, i + 1] = torch.cos(
+                    torch.tensor(pos / (10000**((2 *
+                                                 (i + 1)) / embedding_size))))
+        
+        self.norm = nn.LayerNorm(embedding_size)
+        
+        self.label_num = len(id2labels)
+        self.dropout = nn.Dropout(dropout_rate)
+        self.classifier = nn.Sequential(nn.Linear(embedding_size, self.label_num))
+        self.crf = ConditionalRandomField(num_tags=self.label_num, allowed_transitions=allowed_transitions(id2labels))
+        self.crf.trans_m.data *= 0
     
+    def forward(self, inputs, labels):
+        mask = labels.gt(-1)
+        padding_mask = ~mask
+        
+        outputs = inputs + self.position_encoding.to(inputs.device)
+        outputs = self.norm(outputs)
+        outputs = self.encoder(outputs, src_key_padding_mask=padding_mask)
+        dropout_outputs = self.dropout(outputs)
+        logits = self.classifier(dropout_outputs)
+        
+        if self.training:
+            loss_fct = CrossEntropyLoss(ignore_index=-1)
+            loss = loss_fct(logits.view(-1, self.label_num), labels.view(-1))
+            output = {'loss': loss, 'logits': logits}
+        else:
+            paths, scores = self.crf.viterbi_decode(logits=logits, mask=mask)
+            paths[mask==0] = -1
+            output = {'preds': paths, 'logits': logits}
+            pass
+
+        return output

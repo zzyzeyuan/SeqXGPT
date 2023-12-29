247781296ac3b61361fd0c5c3e754f298916e0dc
Zeyuan Zhao <48199429+zzyzeyuan@users.noreply.github.com>
2023-12-29 16:00:39 +0800

    Update gen_features.py

--- SeqXGPT/ds/gen_features.py
+++ SeqXGPT/ds/gen_features.py
@@ -28,8 +28,8 @@ def access_api(text, api_url, do_generate=False):
         }
         prediction = client.post(api_url,
                                  data=msgpack.packb(post_data),
-                                 timeout=300)
-    # print('=======>prediction status code : ', prediction.status_code)
+                                 timeout=15)
+    print('=======>prediction status code : ', prediction.status_code)
     if prediction.status_code == 200:
         content = msgpack.unpackb(prediction.content)
     else:
